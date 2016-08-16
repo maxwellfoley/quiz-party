f70bc48e377d4ef312486d8f925115ceafc85836
Maxwell Foley <maxwellfoley@wustl.edu>
2016-08-15 21:46:44 -0500

    _

--- public/js/app.js
+++ public/js/app.js
@@ -20,12 +20,7 @@ angular.module("contactsApp", ['ngRoute'])
             })
             .when("/quiz", {
               templateUrl: "quizlet.html",
-              controller: "HelloWorldController",
-              resolve: {
-                  contacts: function(Contacts) {
-                      return Contacts.getContacts();
-                  }
-              }
+              controller: "HelloWorldController"
             })
             .otherwise({
                 redirectTo: "/"
