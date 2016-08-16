368d8be7dbd2b582001892e41b2dbda70591950d
Maxwell Foley <maxwellfoley@wustl.edu>
2016-08-15 21:39:04 -0500

    _

--- public/js/app.js
+++ public/js/app.js
@@ -125,8 +125,6 @@ angular.module("contactsApp", ['ngRoute'])
     })
     .controller('HelloWorldController', function($scope, contacts){
 
-       $scope.contacts = Contacts.getContacts();
-
        $scope.pieces = [
         {title:"tomb of the first emperor of qin",period:"qin dynasty",country:"china",material:"terra cotta",image:"1.jpg"},
         {title:"taotie mask",period:"western zhou dynasty",country:"china",material:"bronze",image:"2.jpg"},
