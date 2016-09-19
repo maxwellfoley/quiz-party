16317c484b9fa9238ee3bd6a324ac37bcd039afe
Maxwell Foley <maxwellfoley@wustl.edu>
2016-09-19 13:16:02 -0500

    _

--- public/js/app.js
+++ public/js/app.js
@@ -177,13 +177,13 @@ angular.module("contactsApp", ['ngRoute'])
             });
         }
     })
-    .controller("ChangeCardsController", function(cards, $scope, $routeParams, Contacts) {
+    .controller("ChangeCardsController", function(cards, $scope, $routeParams, $route, Contacts) {
       console.log("cards " + cards);
       $scope.pieces = cards.data;
 
       $scope.deleteContact = function(contactId) {
           console.log("delete contact function");
-          Contacts.deleteContact(contactId);
+        //  Contacts.deleteContact(contactId);
           console.log("back here");
           /*$scope.stuff = Contacts.getContacts();
           console.log($scope.stuff);
